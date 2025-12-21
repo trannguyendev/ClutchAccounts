b10a7ed33ea84a17f06f082d622c0e7b180328e7
TranNguyen <trannguyen4862@gmail.com>
2025-12-21 10:47:26 +0700

    Create auto login if token is valid and is not expired

---
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/utils/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.kingdomeprotocol.utils;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+private final JwtUtils jwtUtils;
+private final UserDetailsService userDetailServ;
+
+@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String header = request.getHeader("Authorization");
+		if (header != null && header.trim().length() > 0 && header.startsWith("Bearer ")) {
+			String token = header.substring(7);
+			if (jwtUtils.validateToken(token) == true) {
+				String email = jwtUtils.extractEmail(token);
+				if (SecurityContextHolder.getContext().getAuthentication() == null) {
+					UserDetails userDetail = userDetailServ.loadUserByUsername(email);
+					var authToken = new UsernamePasswordAuthenticationToken(userDetail, null, userDetail.getAuthorities()); //Create + add 
+					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));									//auth token 
+					SecurityContextHolder.getContext().setAuthentication(authToken);																//when token is valid
+				}
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+}
