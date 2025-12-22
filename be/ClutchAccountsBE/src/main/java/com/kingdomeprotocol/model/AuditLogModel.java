6e269d63bb1a6c97c15e286d34628aa282f94015
TranNguyen <trannguyen4862@gmail.com>
2025-12-22 22:34:47 +0700

    Create AuditLog Entity

---
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/model/AuditLogModel.java
@@ -0,0 +1,34 @@
+package com.kingdomeprotocol.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "AuditLog")
+public class AuditLogModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("id")
+	int id;
+	@JsonProperty("user_id")
+	int user_id;
+	@JsonProperty("logged_ip")
+	String logged_ip;
+	@JsonProperty("logged_device")
+	String logged_device;
+	@JsonProperty("logged_time")
+	LocalDateTime logged_time;
+}
