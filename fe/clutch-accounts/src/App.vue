d60fd8048268930b84534f47ef88b7b0a03afbf3
TranNguyen <trannguyen4862@gmail.com>
2025-12-22 11:49:06 +0700

    Update: Auto load token if it's available

--- fe/clutch-accounts/src/App.vue
+++ fe/clutch-accounts/src/App.vue
@@ -1,6 +1,41 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Footer from './components/Footer.vue';
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css';
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const token = localStorage.getItem('token');
+if (token){
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response) {
+            const status = error.response.status;
+            if (status >= 400 && status < 500) {
+                toast.error("Error: Your token might be expired, pls login again", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+                route.push('/auth');
+            } else if (status >= 500) {
+                toast.error('Server Error: Please try again later.', {
+                    position: toast.POSITION.TOP_RIGHT
+                });
+            }
+        } else {
+            toast.error('Network Error: Please check your connection.', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+        return Promise.reject(error);
+    }
+);
 </script>
 
 <template>
