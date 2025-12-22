faf6ea673eb2738a2bf443d35bc308bc36d156e6
TranNguyen <trannguyen4862@gmail.com>
2025-12-22 11:15:01 +0700

    Update validate token + adjust expireTime for testing easier

--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/utils/JwtAuthenticationFilter.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/utils/JwtAuthenticationFilter.java
@@ -3,18 +3,15 @@ package com.kingdomeprotocol.utils;
 import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -32,12 +29,12 @@ private final UserDetailsService userDetailServ;
 		String header = request.getHeader("Authorization");
 		if (header != null && header.trim().length() > 0 && header.startsWith("Bearer ")) {
 			String token = header.substring(7);
-			if (jwtUtils.validateToken(token) == true) {
+			if (jwtUtils.validateToken(token)) {
 				String email = jwtUtils.extractEmail(token);
 				if (SecurityContextHolder.getContext().getAuthentication() == null) {
 					UserDetails userDetail = userDetailServ.loadUserByUsername(email);
-					var authToken = new UsernamePasswordAuthenticationToken(userDetail, null, userDetail.getAuthorities()); //Create + add 
-					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));									//auth token 
+					var authToken = new UsernamePasswordAuthenticationToken(userDetail, null, userDetail.getAuthorities()); //Create + add
+					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));									//auth token
 					SecurityContextHolder.getContext().setAuthentication(authToken);																//when token is valid
 				}
 			}
--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/utils/JwtUtils.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/utils/JwtUtils.java
@@ -3,17 +3,20 @@ package com.kingdomeprotocol.utils;
 import java.security.Key;
 import java.util.Date;
 
+import org.springframework.stereotype.Component;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+@Component
 public class JwtUtils {
 	
 	private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-	private final int exprieTime = 1000*60*60*168;
-	
+//	private final int exprieTime = 1000*60*60*168; this is real token for production (7 days until expired)
+	private final int exprieTime = 1000 * 60; //Create 1 min token for testing LoL
 	public String tokenGenerator(String email) {
 		return Jwts.builder().setSubject(email).issuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + exprieTime)).signWith(secretKey).compact();
 	}
