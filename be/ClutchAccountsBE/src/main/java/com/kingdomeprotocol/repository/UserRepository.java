c64e6c3aef5d173c925a1694a4cbd05af4f7bd62
TranNguyen <trannguyen4862@gmail.com>
2026-01-01 23:40:53 +0700

    Update: method to avoid race condition

--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/repository/UserRepository.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/repository/UserRepository.java
@@ -3,10 +3,19 @@ package com.kingdomeprotocol.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kingdomeprotocol.model.UserModel;
+
+import jakarta.persistence.LockModeType;
 
 
 public interface UserRepository extends JpaRepository<UserModel, Integer>{
 Optional<UserModel> findByEmail(String email);
+//Lock pessimistic user
+@Lock(LockModeType.PESSIMISTIC_WRITE)
+@Query(value = "select * from Users where email = :email", nativeQuery = true)
+Optional<UserModel> findUser4Upd(@Param("email") String email);
 }
