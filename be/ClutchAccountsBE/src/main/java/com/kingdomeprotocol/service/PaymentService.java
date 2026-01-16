35f3314e925cbbe31750b058f91054279cf365ae
TranNguyen <trannguyen4862@gmail.com>
2026-01-16 21:41:57 +0700

    Update: Payment Service
    Create: add create success/ cancel log for user

--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/service/PaymentService.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/service/PaymentService.java
@@ -30,7 +30,30 @@ public class PaymentService {
 		transaction.setStatus("PENDING");
 		transRepo.save(transaction);
 	}
-	
+	public void addCancelledTransaction(DTODepositModel depositData) {
+		LocalDateTime currentLocalDateTime = LocalDateTime.now();
+		UserModel user = userServ.loadUserByEmail(depositData.getEmail()).orElseThrow(() -> new RuntimeException("Not found user"));
+		TransactionsModel transaction = new TransactionsModel();
+		transaction.setTransactionUser(user);
+		transaction.setAmount(depositData.getAmount());
+		transaction.setType("DEPOSIT");
+		transaction.setDescrp(depositData.getDescrp());
+		transaction.setCreated_at(currentLocalDateTime);
+		transaction.setStatus("CANCELLED");
+		transRepo.save(transaction);
+	}
+	public void addSuccessTransaction(DTODepositModel depositData) {
+		LocalDateTime currentLocalDateTime = LocalDateTime.now();
+		UserModel user = userServ.loadUserByEmail(depositData.getEmail()).orElseThrow(() -> new RuntimeException("Not found user"));
+		TransactionsModel transaction = new TransactionsModel();
+		transaction.setTransactionUser(user);
+		transaction.setAmount(depositData.getAmount());
+		transaction.setType("DEPOSIT");
+		transaction.setDescrp(depositData.getDescrp());
+		transaction.setCreated_at(currentLocalDateTime);
+		transaction.setStatus("APPROVED");
+		transRepo.save(transaction);
+	}
 	public void approveTransaction(int transaction_id) {
 		TransactionsModel transForApprove = transRepo.findById(transaction_id).orElseThrow(() -> new RuntimeException("Not found transaction"));
 		transForApprove.setStatus("APPROVED");
