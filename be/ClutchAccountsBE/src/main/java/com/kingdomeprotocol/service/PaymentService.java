b3ad7655b96f5020ea2dc48b61bf33bcf16c9daa
TranNguyen <trannguyen4862@gmail.com>
2026-01-18 12:37:59 +0700

    Update: Payment Service
    Create auto create success log with verified webhook + add user balance

--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/service/PaymentService.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/service/PaymentService.java
@@ -42,17 +42,20 @@ public class PaymentService {
 		transaction.setStatus("CANCELLED");
 		transRepo.save(transaction);
 	}
-	public void addSuccessTransaction(DTODepositModel depositData) {
+	public void addSuccessAndDepositTransaction(int userId, String descrp, int amount) {
 		LocalDateTime currentLocalDateTime = LocalDateTime.now();
-		UserModel user = userServ.loadUserByEmail(depositData.getEmail()).orElseThrow(() -> new RuntimeException("Not found user"));
+		UserModel user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("Not found user"));
 		TransactionsModel transaction = new TransactionsModel();
 		transaction.setTransactionUser(user);
-		transaction.setAmount(depositData.getAmount());
+		transaction.setAmount(amount);
 		transaction.setType("DEPOSIT");
-		transaction.setDescrp(depositData.getDescrp());
+		transaction.setDescrp(descrp);
 		transaction.setCreated_at(currentLocalDateTime);
 		transaction.setStatus("APPROVED");
 		transRepo.save(transaction);
+		
+		user.setBalance(user.getBalance() + amount);
+		userRepo.save(user);
 	}
 	public void approveTransaction(int transaction_id) {
 		TransactionsModel transForApprove = transRepo.findById(transaction_id).orElseThrow(() -> new RuntimeException("Not found transaction"));
