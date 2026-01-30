2ecf824602912e7447f1048e5f30a3f4f10dd6d1
MinhQuan-themga <qnguyenminh195@gmail.com>
2026-01-30 21:30:11 +0700

    CRUD voucher

--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/security/MainSecureConfig.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/security/MainSecureConfig.java
@@ -59,6 +59,7 @@ public SecurityFilterChain configSecure(HttpSecurity http, JwtAuthenticationFilt
 		config.requestMatchers("/api/accounts/**").authenticated();
 		config.requestMatchers("/api/news/**").authenticated();
 		config.requestMatchers("/test-api").permitAll();
+		config.requestMatchers("/api/vouchers/**").authenticated();
 	});
 	http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	return http.build();
