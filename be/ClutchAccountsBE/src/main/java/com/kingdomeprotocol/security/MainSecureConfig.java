8a02597381bad492d461a191c666c2e366a05b4c
MinhQuan-themga <qnguyenminh195@gmail.com>
2026-01-30 21:16:38 +0700

    Revert "CRUD cơ bản vouchers"
    This reverts commit ecef6a6560412db8472013caa0595685b3649dbc.

--- be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/security/MainSecureConfig.java
+++ be/ClutchAccountsBE/src/main/java/com/kingdomeprotocol/security/MainSecureConfig.java
@@ -58,7 +58,6 @@ public SecurityFilterChain configSecure(HttpSecurity http, JwtAuthenticationFilt
 		config.requestMatchers("/api/payment/**").permitAll();
 		config.requestMatchers("/api/accounts/**").authenticated();
 		config.requestMatchers("/test-api").permitAll();
-		config.requestMatchers("/api/vouchers/**").authenticated();
 	});
 	http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	return http.build();
